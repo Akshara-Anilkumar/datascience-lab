32e0ee332e819cc3ca64f813a253fe469bcedfa8
Your Name <you@example.com>
2024-10-16 16:07:57 +0530

    cycle5

---
+++ labcycle5/prgrm4.py
@@ -0,0 +1,8 @@
+print("SJC23MCA-2008 : AKSHARA ANILKUMAR")
+print("Batch : MCA 2023-25")
+from nltk import ngrams
+sentence = 'I reside in India'
+n = 3
+trigrams = ngrams(sentence.split(),n)
+for grams in trigrams:
+ print(grams)
